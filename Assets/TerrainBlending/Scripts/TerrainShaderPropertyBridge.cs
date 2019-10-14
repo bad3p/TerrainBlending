4c3cbd6118368901be2caa7e9260d29d2c43e5ed
Alexander Petryaev <a.petryaev@pixonic.com>
2019-10-14 19:04:17 +0300

    F! Utility script transferring terrain properties to global shader properties. F! The shader is still very limited in functionality, it can only fetch terrain textures and mix them with splat map.

---
+++ Assets/TerrainBlending/Scripts/TerrainShaderPropertyBridge.cs
@@ -0,0 +1,180 @@
+﻿
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(Terrain))]
+[ExecuteInEditMode]
+public class TerrainShaderPropertyBridge : MonoBehaviour
+{
+    private static class Uniforms
+    {
+        internal static readonly int _TerrainHeightmap = Shader.PropertyToID("_TerrainHeightmap");
+        internal static readonly int _TerrainNormalmap = Shader.PropertyToID("_TerrainNormalmap");
+        internal static readonly int _TerrainControl = Shader.PropertyToID("_TerrainControl");
+        internal static readonly int _TerrainLightmap = Shader.PropertyToID("_TerrainLightmap");
+        internal static readonly int _TerrainLightmap_ST = Shader.PropertyToID("_TerrainLightmap_ST");
+        internal static readonly int _TerrainHeightmapDoubleScale = Shader.PropertyToID("_TerrainHeightmapDoubleScale");
+        internal static readonly int _TerrainSize = Shader.PropertyToID("_TerrainSize");
+        internal static readonly int _TerrainPos = Shader.PropertyToID("_TerrainPos");
+        internal static readonly int[] _TerrainSplat =
+        {
+            Shader.PropertyToID("_TerrainSplat0"),
+            Shader.PropertyToID("_TerrainSplat1"),
+            Shader.PropertyToID("_TerrainSplat2"),
+            Shader.PropertyToID("_TerrainSplat3")
+        };
+        internal static readonly int[] _TerrainSplat_ST =
+        {
+            Shader.PropertyToID("_TerrainSplat0_ST"),
+            Shader.PropertyToID("_TerrainSplat1_ST"),
+            Shader.PropertyToID("_TerrainSplat2_ST"),
+            Shader.PropertyToID("_TerrainSplat3_ST")
+        };
+        internal static readonly int[] _TerrainNormal =
+        {
+            Shader.PropertyToID("_TerrainNormal0"),
+            Shader.PropertyToID("_TerrainNormal1"),
+            Shader.PropertyToID("_TerrainNormal2"),
+            Shader.PropertyToID("_TerrainNormal3")
+        };
+        internal static readonly int[] _TerrainNormalScale =
+        {
+            Shader.PropertyToID("_TerrainNormalScale0"),
+            Shader.PropertyToID("_TerrainNormalScale1"),
+            Shader.PropertyToID("_TerrainNormalScale2"),
+            Shader.PropertyToID("_TerrainNormalScale3")
+        };
+        internal static readonly int[] _TerrainMetallic =
+        {
+            Shader.PropertyToID("_TerrainMetallic0"),
+            Shader.PropertyToID("_TerrainMetallic1"),
+            Shader.PropertyToID("_TerrainMetallic2"),
+            Shader.PropertyToID("_TerrainMetallic3")
+        };
+        internal static readonly int[] _TerrainSmoothness =
+        {
+            Shader.PropertyToID("_TerrainSmoothness0"),
+            Shader.PropertyToID("_TerrainSmoothness1"),
+            Shader.PropertyToID("_TerrainSmoothness2"),
+            Shader.PropertyToID("_TerrainSmoothness3")
+        };
+    }
+    
+    private Terrain _cachedTerrain;
+    private Vector3 _cachedTerrainSize;
+    private TerrainLayer[] _cachedTerrainLayers = new TerrainLayer[0];
+    private Texture2D[] _cachedAlphamapTextures = new Texture2D[0];
+    private LightmapData[] _cachedLightmaps = new LightmapData[0];
+    
+    void Awake()
+    {
+        _cachedTerrain = GetComponent<Terrain>();
+        _cachedTerrainSize = _cachedTerrain.terrainData.size;
+        _cachedTerrainLayers = _cachedTerrain.terrainData.terrainLayers;
+        _cachedAlphamapTextures = _cachedTerrain.terrainData.alphamapTextures;
+        _cachedLightmaps = LightmapSettings.lightmaps;
+    }
+
+    void OnEnable()
+    {
+        Shader.EnableKeyword("_TERRAIN_BLENDING");
+    }
+
+    void OnDisable()
+    {
+        Shader.DisableKeyword("_TERRAIN_BLENDING");
+    }
+
+    void OnDestroy()
+    {
+        Shader.DisableKeyword("_TERRAIN_BLENDING");
+    }
+
+    Vector4 GetSplatTextureScaleOffset(int terrainLayer)
+    {
+#if UNITY_EDITOR
+        if (EditorApplication.isPlaying)
+#else
+        if( Application.isPlaying )
+#endif
+        {
+            Vector3 tileSize = _cachedTerrainLayers[terrainLayer].tileSize;
+            Vector3 tileOffset = _cachedTerrainLayers[terrainLayer].tileOffset;
+            return new Vector4(_cachedTerrainSize.x / tileSize.x, _cachedTerrainSize.z / tileSize.y, tileOffset.x, tileOffset.y);
+        }
+        else
+        {
+            Vector3 terrainSize = _cachedTerrain.terrainData.size;
+            Vector3 tileSize = _cachedTerrain.terrainData.terrainLayers[terrainLayer].tileSize;
+            Vector3 tileOffset = _cachedTerrain.terrainData.terrainLayers[terrainLayer].tileOffset;
+            return new Vector4(terrainSize.x / tileSize.x, terrainSize.z / tileSize.y, tileOffset.x, tileOffset.y);
+        }
+    }
+
+    void Update()
+    {
+        Shader.SetGlobalTexture( Uniforms._TerrainHeightmap, _cachedTerrain.terrainData.heightmapTexture );
+        Shader.SetGlobalTexture( Uniforms._TerrainNormalmap, _cachedTerrain.normalmapTexture );
+        Shader.SetGlobalVector( Uniforms._TerrainHeightmapDoubleScale, _cachedTerrain.terrainData.heightmapScale * 2.0f );
+        Shader.SetGlobalVector( Uniforms._TerrainSize, _cachedTerrain.terrainData.size );
+        Shader.SetGlobalVector( Uniforms._TerrainPos, _cachedTerrain.transform.position );
+        
+#if UNITY_EDITOR
+        if (EditorApplication.isPlaying)
+#else
+        if( Application.isPlaying )
+#endif
+        {
+            for (int i = 0; i < _cachedTerrainLayers.Length; i++)
+            {
+                Shader.SetGlobalTexture( Uniforms._TerrainSplat[i], _cachedTerrainLayers[i].diffuseTexture );
+                Shader.SetGlobalVector( Uniforms._TerrainSplat_ST[i], GetSplatTextureScaleOffset(i) );
+                Shader.SetGlobalTexture( Uniforms._TerrainNormal[i], _cachedTerrainLayers[i].normalMapTexture );
+                Shader.SetGlobalFloat( Uniforms._TerrainNormalScale[i], _cachedTerrainLayers[i].normalScale );
+                Shader.SetGlobalFloat( Uniforms._TerrainMetallic[i], _cachedTerrainLayers[i].metallic );
+                Shader.SetGlobalFloat(Uniforms._TerrainSmoothness[i], _cachedTerrainLayers[i].smoothness);
+            }
+
+            Shader.SetGlobalTexture( Uniforms._TerrainControl, _cachedAlphamapTextures[0] );
+            
+            if (_cachedLightmaps.Length > 0)
+            {
+                Shader.SetGlobalVector( Uniforms._TerrainLightmap_ST, _cachedTerrain.lightmapScaleOffset );
+                Shader.SetGlobalTexture( Uniforms._TerrainLightmap, _cachedLightmaps[_cachedTerrain.lightmapIndex].lightmapColor );
+            }
+            else
+            {
+                Shader.SetGlobalVector( Uniforms._TerrainLightmap_ST, new Vector4(1,1,0,0) );
+                Shader.SetGlobalTexture( Uniforms._TerrainLightmap, Texture2D.whiteTexture );
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _cachedTerrainLayers.Length; i++)
+            {
+                Shader.SetGlobalTexture( Uniforms._TerrainSplat[i], _cachedTerrain.terrainData.terrainLayers[i].diffuseTexture );
+                Shader.SetGlobalVector( Uniforms._TerrainSplat_ST[i], GetSplatTextureScaleOffset(i) );
+                Shader.SetGlobalTexture( Uniforms._TerrainNormal[i], _cachedTerrain.terrainData.terrainLayers[i].normalMapTexture );
+                Shader.SetGlobalFloat( Uniforms._TerrainNormalScale[i], _cachedTerrain.terrainData.terrainLayers[i].normalScale );
+                Shader.SetGlobalFloat( Uniforms._TerrainMetallic[i], _cachedTerrain.terrainData.terrainLayers[i].metallic );
+                Shader.SetGlobalFloat(Uniforms._TerrainSmoothness[i], _cachedTerrain.terrainData.terrainLayers[i].smoothness);
+            }
+
+            Shader.SetGlobalTexture( Uniforms._TerrainControl, _cachedTerrain.terrainData.alphamapTextures[0] );
+            
+            if (LightmapSettings.lightmaps.Length > 0)
+            {
+                Shader.SetGlobalVector( Uniforms._TerrainLightmap_ST, _cachedTerrain.lightmapScaleOffset );
+                Shader.SetGlobalTexture( Uniforms._TerrainLightmap, LightmapSettings.lightmaps[_cachedTerrain.lightmapIndex].lightmapColor );
+            }
+            else
+            {
+                Shader.SetGlobalVector( Uniforms._TerrainLightmap_ST, new Vector4(1,1,0,0) );
+                Shader.SetGlobalTexture( Uniforms._TerrainLightmap, Texture2D.whiteTexture );
+            }
+        }
+    }
+}
---
+++ Assets/TerrainBlending/Shaders/TerrainBlendedStandard.shader
@@ -0,0 +1,116 @@
+﻿Shader "Standart/Terrain-Blended"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma exclude_renderers gles            
+            #pragma multi_compile_fog
+            #pragma multi_compile _ _TERRAIN_BLENDING             
+
+            #include "UnityCG.cginc"
+            
+            sampler2D _TerrainHeightmap;
+            sampler2D _TerrainNormalmap;
+            sampler2D _TerrainControl;
+            float4 _TerrainControl_ST;
+            sampler2D _TerrainLightmap;
+            float4 _TerrainLightmap_ST;
+            float4 _TerrainHeightmapDoubleScale;
+            float4 _TerrainSize;
+            float4 _TerrainPos;        
+            sampler2D _TerrainSplat0, _TerrainSplat1, _TerrainSplat2, _TerrainSplat3;
+            float4 _TerrainSplat0_ST, _TerrainSplat1_ST, _TerrainSplat2_ST, _TerrainSplat3_ST;        
+            sampler2D _TerrainNormal0, _TerrainNormal1, _TerrainNormal2, _TerrainNormal3;
+            float _TerrainNormalScale0, _TerrainNormalScale1, _TerrainNormalScale2, _TerrainNormalScale3;            
+            float _TerrainMetallic0, _TerrainMetallic1, _TerrainMetallic2, _TerrainMetallic3;
+            float _TerrainSmoothness0, _TerrainSmoothness1, _TerrainSmoothness2, _TerrainSmoothness3;
+            
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            half _Glossiness;
+            half _Metallic;  
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float3 worldPos : TEXCOORD1;
+                UNITY_FOG_COORDS(2)
+            };
+
+            float2 TerrainUVs(float3 worldPos)
+            {
+                return ( worldPos.xz - _TerrainPos.xz ) / _TerrainSize.xz;
+            }
+        
+            half TerrainHeight(half2 terrainUV)
+            {
+                return _TerrainPos.y + UnpackHeightmap( tex2Dlod( _TerrainHeightmap, half4(terrainUV,0,0) ) ) * _TerrainHeightmapDoubleScale.y;
+            }
+        
+            half3 TerrainNormal(half2 terrainUV)
+            {
+                return tex2Dlod( _TerrainNormalmap, half4(terrainUV,0,0) ).xyz * 2.0 - 1.0;
+            }
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.worldPos = mul( unity_ObjectToWorld, v.vertex).xyz;
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            half4 frag (v2f i) : SV_Target
+            {
+                float2 splatUV = TerrainUVs( i.worldPos );
+                half4 splatControl = tex2D( _TerrainControl, splatUV );
+                half weight = dot( splatControl, half4(1,1,1,1) );
+                splatControl /= (weight + 1e-3f);
+                
+                float2 uvSplat0 = TRANSFORM_TEX( splatUV, _TerrainSplat0 );
+                float2 uvSplat1 = TRANSFORM_TEX( splatUV, _TerrainSplat1 );
+                float2 uvSplat2 = TRANSFORM_TEX( splatUV, _TerrainSplat2 );
+                float2 uvSplat3 = TRANSFORM_TEX( splatUV, _TerrainSplat3 );
+                
+                half4 mixedDiffuse = splatControl.r * tex2D( _TerrainSplat0, uvSplat0 );
+                mixedDiffuse += splatControl.g * tex2D( _TerrainSplat1, uvSplat1 );
+                mixedDiffuse += splatControl.b * tex2D( _TerrainSplat2, uvSplat2 );
+                mixedDiffuse += splatControl.a * tex2D( _TerrainSplat3, uvSplat3 );
+                
+                half3 mixedNormal = UnpackNormalWithScale( tex2D( _TerrainNormal0, uvSplat0 ), _TerrainNormalScale0 ) * splatControl.r;
+                mixedNormal += UnpackNormalWithScale( tex2D( _TerrainNormal1, uvSplat1 ), _TerrainNormalScale1 ) * splatControl.g;
+                mixedNormal += UnpackNormalWithScale( tex2D( _TerrainNormal2, uvSplat2 ), _TerrainNormalScale2 ) * splatControl.b;
+                mixedNormal += UnpackNormalWithScale( tex2D( _TerrainNormal3, uvSplat3 ), _TerrainNormalScale3 ) * splatControl.a;
+                mixedNormal.z += 1e-5f;
+                return half4( mixedDiffuse.xyz,1 );
+            
+                fixed4 col = tex2D( _MainTex, i.uv );
+                UNITY_APPLY_FOG( i.fogCoord, col );
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
